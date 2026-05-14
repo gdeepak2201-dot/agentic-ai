5492a7814670da4b22901b5bbf2dc93775eb4ad7
gdeepak2201-dot <gdeepak2201@gmail.com>
2026-05-14 18:49:05 +0530

    Fix Vercel 404 and API routing issues

--- api/index.js
+++ api/index.js
@@ -94,7 +94,7 @@ app.post('/api/diagnose', async (req, res) => {
 
 // Serve frontend in production or if deployed as one unit
 app.use(express.static(path.join(__dirname, '../../dist')));
-app.get('*', (req, res) => {
+app.get('(.*)', (req, res) => {
   res.sendFile(path.join(__dirname, '../../dist/index.html'));
 });
 
